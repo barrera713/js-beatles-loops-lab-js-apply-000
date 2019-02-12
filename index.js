0ba2e263f2f176bb242984e0e989f7ec868dcebc
Carlos Barrera <barrera9696@gmail.com>
2019-02-12 03:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  const str = musicians[i] +  ' plays ' + instruments[i];
+  array.push(str);
+  }
+  return array;
+  }
+
+function johnLennonFacts(facts) {
+  var i=[];
+  var count=0;
+  while(count<facts.length) {
+    i.push(facts[count] + '!!!');
+    count++;
+}
+  return i;
+}
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+array.push('I love the Beatles!');
+num++;
+} while (num < 15);
+return array;
+}
+
+  
+
+  
+
+  
+
+
+
+
